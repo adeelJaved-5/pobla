c7a5b32fd008bf74713b2e59ad3385d598e7b30d
Adeel Javed <javedadeel52@gmail.com>
2025-12-08 13:31:57 +0500

    update

--- src/components/layout/Welcome.tsx
+++ src/components/layout/Welcome.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 
 import Logo1 from "@/assets/popup/intro1.png";
-import Logo2 from "@/assets/popup/intro2.png";
 
 const Welcome = () => {
     return (
@@ -11,10 +10,6 @@ const Welcome = () => {
             <div className="relative w-full max-w-md rounded-2xl shadow-2xl p-10 flex flex-col items-center justify-center gap-16 py-24 gradient-bg">
                 <Image
                     src={Logo1}
-                    alt="logo"
-                />
-                <Image
-                    src={Logo2}
                     alt="logo"
                 />
             </div>
